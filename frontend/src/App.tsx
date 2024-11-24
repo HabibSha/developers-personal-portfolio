de637abe0f1bffcf870c02a4d352f0a8346cd0fc
habibsha <habibshahalahi7424@gmail.com>
2024-11-24 22:52:02 +0600

    Create new intro component and compressed all medias for optimization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 import Routers from "./routers/Routers";
+import Intro from "./components/Intro/Intro";
 
 function App() {
+  const [showIntro, setShowIntro] = useState(false);
+
+  useEffect(() => {
+    const hasSeenIntro = localStorage.getItem("hasSeenIntro");
+
+    if (!hasSeenIntro) {
+      setShowIntro(true); // Show intro
+      localStorage.setItem("hasSeenIntro", "true"); // Mark intro as seen
+      // Automatically hide the intro after 4 seconds
+      setTimeout(() => {
+        setShowIntro(false);
+      }, 6000);
+    }
+  }, []);
+
   return (
     <>
-      <Routers />
+      {showIntro && <Intro />}
+      {!showIntro && <Routers />}
     </>
   );
 }
---
+++ frontend/src/components/Intro/Intro.tsx
@@ -0,0 +1,12 @@
+import "./intro.module.css";
+
+const Intro = () => {
+  return (
+    <div className="intro-overlay">
+      <h1 className="text-center text-white">Welcome to My Portfolio</h1>
+      {/* You can add an animation or styling here */}
+    </div>
+  );
+};
+
+export default Intro;
